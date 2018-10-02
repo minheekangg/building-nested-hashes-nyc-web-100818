16bf19377d24872f0157436eb198f01baea0b291
Min Hee  Park <minheekangg@gmail.com>
2018-10-02 15:57:30 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,19 +4,16 @@ def third_challenge
       :patriarch => {
         :name => "Lord Montague"
         :age => "53"
-        #your key/value pairs here
         },
       :matriarch => {
         name: "Lady Montague",
         age: "54"
-        #your key/value pairs here
         },
       :hero => {
-      name: "Romeo",
-      age: "15",
-      status: "alive"
-        #your key/value pairs here
-        },
+        name: "Romeo",
+        age: "15",
+        status: "alive"
+          },
       :hero_friends => []
    },
    :capulet => {
