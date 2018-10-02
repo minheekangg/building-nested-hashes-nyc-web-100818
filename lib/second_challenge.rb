f4e414590e7841e3018606c7153fb2f982edca32
Min Hee  Park <minheekangg@gmail.com>
2018-10-02 15:54:33 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
       },
     :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       #your key/value pairs here!
       }}
 
